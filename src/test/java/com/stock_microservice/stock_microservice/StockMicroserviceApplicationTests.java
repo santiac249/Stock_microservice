bd7aab45670fffbc5fe4b544c9bd54e9f4674f98
santiac249 <101884989+santiac249@users.noreply.github.com>
2024-08-23 16:24:28 -0500

    Inicio

---
+++ src/test/java/com/stock_microservice/stock_microservice/StockMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock_microservice.stock_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
